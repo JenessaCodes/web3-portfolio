ea25504c987ef7a3934adf83c76c15a4b4cb5e80
Jenessa Codes <87744322+JenessaCodes@users.noreply.github.com>
2022-04-30 20:38:36 -0700

    Customize

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from "../assets/profile.png";
 
 const Header = () => {
-    return (
-        <section className='header'>
-            <img src={profile} alt="John Doe" />
+  return (
+    <section className="header">
+      <img src={profile} alt="John Doe" />
 
-            <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
-                <p>Blockchain Developer</p>
-                <button className='button'>Get In Touch</button>
-            </div>
-        </section>
-    );
-}
+      <div className="header__content">
+        <h1>Hi, I'm Jenessa Codes</h1>
+        <p>Blockchain Developer</p>
+        <button className="button">Get In Touch</button>
+      </div>
+    </section>
+  );
+};
 
 export default Header;
